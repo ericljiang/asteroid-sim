92a4a0ddd329cb8aec44cdf89087e0b9967fbb66
Eric Jiang <eric.l.jiang@duke.edu>
2015-07-16 01:18:18 -0700

    Added variables

--- src/main/Body.java
+++ src/main/Body.java
@@ -3,8 +3,14 @@ package main;
 public abstract class Body {
 	public float mass;
 	public float radius;
+	public float a; // Semi-major axis
+	public float b; // Semi-minor axis
+	public float rotPeriod;
+	public float rotation; // could be replaced with getRotation() calculation
 	
 	public void update() {
+		// Update position
+		// Update rotation
 		
 	}
 }
