ddd64279a2d7c6ac8f3fdd800f6ca878401c4f49
Eric Jiang <eric.l.jiang@duke.edu>
2015-07-20 19:10:20 -0700

    Added solar system framework

--- src/main/SolarSystem.java
+++ src/main/SolarSystem.java
@@ -1,5 +1,19 @@
 package main;
 
+import java.util.ArrayList;
+
 public class SolarSystem {
+	public Sun mySun;
+	public ArrayList<Planet> myPlanets;
+	
+	public SolarSystem() {
+		mySun = new Sun();
+		myPlanets = new ArrayList<Planet>();
+	}
+	
+	public void update() {
+		mySun.update();
+		
+	}
 
 }
