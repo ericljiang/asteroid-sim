da43653ca3e52794933cd7f28d82679193010419
Eric Jiang <eric.l.jiang@duke.edu>
2015-07-21 11:50:25 -0700

    Added main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,15 @@
 package main;
 
 public class Main {
-
+	public SolarSystem mySolarSystem;
+	
+	public Main() {
+		mySolarSystem = new SolarSystem();
+	}
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.mySolarSystem = new SolarSystem();
+		
+	}
 }
