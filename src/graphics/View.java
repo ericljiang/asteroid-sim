e892c7603766a7fe2548d85ff8e89421b9560d82
Eric Jiang <eric.l.jiang@duke.edu>
2015-10-06 16:30:00 -0400

    Added openGL framework

---
+++ src/graphics/View.java
@@ -0,0 +1,24 @@
+package graphics;
+
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+
+import com.jogamp.opengl.util.gl2.GLUT;
+
+import framework.Scene;
+
+public class View extends Scene {
+
+	@Override
+	public void display(GL2 gl, GLU glu, GLUT glut) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCamera(GL2 gl, GLU glu, GLUT glut) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,15 @@
 package main;
 
-import math.Kepler;
-import math.Point;
+import framework.JOGLFrame;
+import graphics.View;
 
 public class Main {
 	public static void main(String[] args) {
-		Simulation simulation = new Simulation();
+		new JOGLFrame(new View());
+		/*Simulation simulation = new Simulation();
 		for (int i = 0; i < 10; i++) {
 			simulation.update();
 			System.out.println("update");
-		}
+		}*/
 	}
 }
