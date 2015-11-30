7897d301c59925af5a75309ea8427ce0f1da3034
Eric Jiang <eric.l.jiang@duke.edu>
2015-11-30 10:59:32 -0500

    Reads from .csv

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,21 @@
 package main;
 
-import astro.Satellite;
+import java.io.FileNotFoundException;
+
 import astro.SolarSystem;
 import framework.JOGLFrame;
 import graphics.View;
+import utility.FileIO;
 
 public class Main {
 	public static void main(String[] args) {
-		SolarSystem solar = new SolarSystem();
-		Satellite earth = new Satellite(5.97237e24, 6371.0, 0.99726968,
-										149598261, 0.0167, 
-										Math.toRadians(-11.26064), 
-										Math.toRadians(102.947), 
-										Math.toRadians(7.155), solar.getSun());
-		Satellite jupiter = new Satellite(1.8986e27, 69911, 0.413542, 778547200,
-										  0.048775, Math.toRadians(100.492),
-										  Math.toRadians(275.066),
-										  Math.toRadians(6.09), solar.getSun());
-		solar.addPlanet(earth);
-		solar.addPlanet(jupiter);
-		View view = new View(solar);
-		new JOGLFrame(view);
+		try {
+			SolarSystem solar = FileIO.readCSV("configs/solarsystem.csv");
+			View view = new View(solar);
+			new JOGLFrame(view);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
